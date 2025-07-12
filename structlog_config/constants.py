2e2e0e9c07bfe80a0ba3e3d8b7278b76619cefa4
Michael Bianco <mike@mikebian.co>
2025-07-12 15:02:06 -0600

    docs: clarify description of PYTHONASYNCIODEBUG constant
    Generated-by: aiautocommit

--- structlog_config/constants.py
+++ structlog_config/constants.py
@@ -4,7 +4,7 @@ import os
 from decouple import config
 
 PYTHONASYNCIODEBUG = config("PYTHONASYNCIODEBUG", default=False, cast=bool)
-"this is a builtin py constant, we check for it to ensure we don't silence this log level"
+"this is a builtin env var, we check for it to ensure we don't silence this log level"
 
 NO_COLOR = "NO_COLOR" in os.environ
 "support NO_COLOR standard https://no-color.org"
